2fb6fbcb97046b6ac41d318f2f1a35908f8b4187
mczhuge <64179323+mczhuge@users.noreply.github.com>
2021-04-16 19:53:07 +0800

    Update get_raw_dataset.sh

--- scripts/dataset/get_raw_dataset.sh
+++ scripts/dataset/get_raw_dataset.sh
@@ -20,9 +20,18 @@ wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBE
 #wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/raw_data/roi_demo/
 
 #Pytorch tensor dataset
-wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/raw_data/extracted_images.tar.gz
-wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/raw_data/full_train_info.txt
-wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/raw_data/full_valid_info.txt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/fashiongen_pretrain_train_feats_1.pt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/fashiongen_pretrain_train_feats_2.pt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/fashiongen_pretrain_train_feats_3.pt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/fashiongen_pretrain_train_feats_4.pt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/fashiongen_pretrain_train_feats_5.pt
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/tsv_to_pt_1000.py
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/datasets/tsv_to_pt_all_split.py
 
+
+
+#Kaleido Patch Generator
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/preprocess/KaleidoBERT-patch-1106-train.py
+#wget http://icbu-ensa-sc.oss-cn-zhangjiakou.aliyuncs.com/mingchen.zgmc/KaleidoBERT_TF_CODE/datasets/torch_dataset/preprocess/KaleidoBERT-patch-1106-valid.py 
 
 echo "Done..."
